d8a3edff7a3783bcf6903f79ff851b3ac3c91b1e
anant-c <canant448@gmail.com>
2026-01-07 00:16:05 +0530

    dockerfile

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,7 @@
-const nextConfig = {}
+import type { NextConfig } from "next";
 
-module.exports = nextConfig
+const nextConfig: NextConfig = {
+  output: 'standalone',
+};
 
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
-// postcss.config.js
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+export default nextConfig;
